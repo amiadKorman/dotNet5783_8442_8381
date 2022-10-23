e6919d0bd6ccf8508e6a2facc5bebd1135f67bd5
ron hodadedi <ronchaim318@gmail.com>
2022-10-23 17:46:13 +0300

    add new entitys

--- dotNet5783_8842_8381/DalFacade/DO/Product.cs
+++ dotNet5783_8842_8381/DalFacade/DO/Product.cs
@@ -5,6 +5,10 @@
 /// </summary>
 public struct Product
 {
-   
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public double Praice { get; set; }
+    public Category Category { get; set; }
+    public int InStock { get; set; }
 
 }
