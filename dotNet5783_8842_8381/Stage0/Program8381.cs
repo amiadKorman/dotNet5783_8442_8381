843298fb1c39a5724ebffd0b4665524d827ba2d5
ron hodadedi <ronchaim318@gmail.com>
2022-10-23 15:13:32 +0300

    add new cs file

---
+++ dotNet5783_8842_8381/Stage0/Program8381.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        
+
+        static partial void Welcome8381()
+        {
+            Console.Write("I am also here!");
+            
+        }
+    }
+}
