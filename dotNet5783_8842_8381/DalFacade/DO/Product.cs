b694a8a1693e42e82e6069d721a339c6d51697b7
ron hodadedi <ronchaim318@gmail.com>
2022-10-23 17:28:00 +0300

    add new flie Product.cs

--- dotNet5783_8842_8381/DalFacade/DO/Product.cs
+++ dotNet5783_8842_8381/DalFacade/DO/Product.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace DO
+namespace DO;
+
+
+/// <summary>
+/// 
+/// </summary>
+public struct Product
 {
-    internal class Product
-    {
-    }
 }
