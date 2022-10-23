6a6fb5bc9cad9d00a714f0bf1a8e3d875df47547
ron hodadedi <ronchaim318@gmail.com>
2022-10-23 15:15:05 +0300

    add namespace for the file

--- dotNet5783_8842_8381/Stage0/Program8442.cs
+++ dotNet5783_8842_8381/Stage0/Program8442.cs
@@ -1,18 +1,21 @@
-﻿partial class Program
+﻿namespace Stage0
 {
-    private static void Main(string[] args)
+    partial class Program
     {
-        Welcome8442();
-        Welcome8381();
-        Console.ReadKey();
-    }
+        private static void Main(string[] args)
+        {
+            Welcome8442();
+            Welcome8381();
+            Console.ReadKey();
+        }
 
-    static partial void Welcome8381();
+        static partial void Welcome8381();
 
-    private static void Welcome8442()
-    {
-        Console.Write("Enter your name: ");
-        string userName = Console.ReadLine();
-        Console.WriteLine("{0}, welcome to my first console application", userName);
+        private static void Welcome8442()
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
     }
 }
