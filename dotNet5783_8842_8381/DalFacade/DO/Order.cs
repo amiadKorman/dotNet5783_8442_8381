f7dd97278eebec708e33ad53c6829a84488a349c
Amiad Korman <amiadk16@gmail.com>
2022-10-23 18:06:49 +0300

    Add properties of order

--- dotNet5783_8842_8381/DalFacade/DO/Order.cs
+++ dotNet5783_8842_8381/DalFacade/DO/Order.cs
@@ -1,8 +1,39 @@
-﻿namespace DO;
+﻿using System.ComponentModel;
+using static DO.Enums;
+
+namespace DO;
 
 /// <summary>
 /// Strucure for Order
 /// </summary>
 public struct Order
 {
+    /// <summary>
+    /// Unique order ID
+    /// </summary>
+    public int ID { get; set; }
+    /// <summary>
+    /// Customer name
+    /// </summary>
+    public string CustomerName { get; set; }
+    /// <summary>
+    /// Customer email
+    /// </summary>
+    public string CustomerEmail { get; set; }
+    /// <summary>
+    /// Customer address
+    /// </summary>
+    public string CustomerAdressl { get; set; }
+    /// <summary>
+    /// Order date
+    /// </summary>
+    public DateTime OrderDate { get; set; }
+    /// <summary>
+    /// Ship date
+    /// </summary>
+    public DateTime ShipDate { get; set; }
+    /// <summary>
+    /// Delivery date
+    /// </summary>
+    public DateTime DeliveryDate { get; set; }
 }
