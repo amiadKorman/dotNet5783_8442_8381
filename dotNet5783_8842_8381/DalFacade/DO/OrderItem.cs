1268258422a265539b3fba80babef08c702e1e60
ron hodadedi <ronchaim318@gmail.com>
2022-10-23 18:38:31 +0300

    OrderItem.cs

--- dotNet5783_8842_8381/DalFacade/DO/OrderItem.cs
+++ dotNet5783_8842_8381/DalFacade/DO/OrderItem.cs
@@ -1,12 +1,36 @@
-﻿namespace DO;
+﻿using static DO.Enums;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace DO;
 
 /// <summary>
 /// Strucure for OrderItems
 /// </summary>
 public struct OrderItem
 {
+    /// <summary>
+    /// ID of procuct
+    /// </summary>
     public int ProductId { get; set; }
+
+    /// <summary>
+    /// Name of OrderName
+    /// </summary>
     public int OrderName { get; set; }
+
+    /// <summary>
+    /// Praice of the order
+    /// </summary>
     public double Praice { get; set; }
+
+    /// <summary>
+    /// Amount of product in the order
+    /// </summary>
     public int Amount { get; set; }
+
+    public override string ToString() => $@"
+     Product ID={ProductId}: {OrderName}, 
+     Price: {Praice}
+     Amount in order: {Amount}";
 }
