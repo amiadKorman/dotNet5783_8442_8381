373911aebbcd8bdb9db386bd73b68787e21baf51
Amiad Korman <amiadk16@gmail.com>
2022-10-23 14:50:21 +0300

    Initial code + Welcome function

---
+++ dotNet5783_8842_8381/Stage0/Program8442.cs
@@ -0,0 +1,18 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome8442();
+        Welcome8381();
+        Console.ReadKey();
+    }
+
+    static partial void Welcome8381();
+
+    private static void Welcome8442()
+    {
+        Console.Write("Enter your name: ");
+        string userName = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", userName);
+    }
+}
