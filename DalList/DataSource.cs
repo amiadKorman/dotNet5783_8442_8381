b7baff30714293b3f4715d4788f31d81b7f152ac
Amiad Korman <amiadk16@gmail.com>
2022-10-27 00:55:35 +0300

    DataSource arrays definition

--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -1,6 +1,27 @@
 ﻿
+using DO;
+
 namespace Dal;
 
 internal static class DataSource
 {
+    /// <summary>
+    /// Random number for initialization
+    /// </summary>
+    static readonly Random RandomNumber = new();
+
+    /// <summary>
+    /// Array of orders
+    /// </summary>
+    internal static Order[] orders = new Order[100];
+
+    /// <summary>
+    /// Array of order items
+    /// </summary>
+    internal static OrderItem[] orderItems = new OrderItem[200];
+
+    /// <summary>
+    /// Array of products
+    /// </summary>
+    internal static Product[] products = new Product[50];  
 }
