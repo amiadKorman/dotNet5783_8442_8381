05377db0bab7c40908406cdeeb6a69cc264a4dbf
ron hodadedi <ronchaim318@gmail.com>
2022-10-27 20:40:30 +0300

    Add class for Safe Input

---
+++ DalTest/SafeInput.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Dal
+{
+    class SafeInput
+    {
+        /// <summary>
+        /// Input an integer value in safe way. if there is an invalid input - try again.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static int IntegerInput(string message = "")
+        {
+            Console.Write(message);
+            int num = 0;
+            return int.TryParse(Console.ReadLine(), out num) ? num : IntegerInput("Invalid input. Please enter only an integer value:\n");
+        }
+
+
+        /// <summary>
+        /// Input a rational value in safe way. if there is an invalid input - try again.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static double DoubleInput(string message = "")
+        {
+            Console.Write(message);
+            double num = 0.0;
+            return double.TryParse(Console.ReadLine(), out num) ? num : DoubleInput("Invalid input. Please enter only an integer value:\n");
+        }
+
+
+    }
+}
