f7b45a6b3c8990e5a63269d26384fab59bdce71f
Amiad Korman <amiadk16@gmail.com>
2022-10-27 10:59:11 +0300

    Add Config and first initializations(not final)

--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -23,5 +23,79 @@ internal static class DataSource
     /// <summary>
     /// Array of products
     /// </summary>
-    internal static Product[] products = new Product[50];  
+    internal static Product[] products = new Product[50];
+
+    internal static class Config
+    {
+        internal static int productsLastIndex = 0;
+        internal static int orderItemsLastIndex = 0;
+        internal static int ordersLastIndex = 0;
+
+        private static int OrderItemID { get; } = 0;
+        private static int OrderID { get; } = 0;
+    }
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    static DataSource()
+    {
+        s_Initialize();
+    }
+
+    private static void s_Initialize()
+    {
+        InitializeProducts();
+        InitializeOrderItems();
+        InitializeOrders();
+    }
+    private static void InitializeProducts()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            products[i] = new Product
+            {
+                ID = 100000 + i,
+                Name = "something",
+                Price = 100,
+                Category = Category.Tech,
+                InStock = 10
+            };
+        }
+    }
+
+    private static void InitializeOrderItems()
+    {
+        for (int i = 0; i < 40; i++)
+        {
+            orderItems[i] = new OrderItem
+            {
+                ProductID = i,
+                OrderID = 100,
+                Price = 100,
+                Amount = 3,
+            };
+        }
+    }
+
+    private static void InitializeOrders()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            orders[i] = new Order 
+            { 
+                ID = i,
+                CustomerName = "a",
+                CustomerEmail =  "harta@gmail.com",
+                CustomerAdress = "Jerus",
+                OrderDate = DateTime.MinValue,
+                ShipDate = DateTime.MinValue,
+                DeliveryDate = DateTime.MinValue
+            };
+        }
+    }
+
+
+
+
 }
