f2955cee63e40aef5030f04becb2ff1c0fe1ee87
ron hodadedi <ronchaim318@gmail.com>
2022-10-28 08:58:20 +0300

    cretae menu for order fanctions

---
+++ DalTest/MenuOfOrder.cs
@@ -0,0 +1,108 @@
+﻿using DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    public class MenuOfOrder
+    {
+        #region Add New Order
+        public static void AddNewOrder()
+        {
+            Order NewOrder = new Order();
+            Console.WriteLine("for add a new Order, please fill in the following data:");
+
+
+            NewOrder.ID = SafeInput.IntegerInput("ID: ");
+            Console.WriteLine("Name: ");
+            NewOrder.CustomerName = Console.ReadLine();
+            Console.WriteLine("Email: ");
+            NewOrder.CustomerEmail = Console.ReadLine();
+            Console.WriteLine("Adress: ");
+            NewOrder.CustomerEmail = Console.ReadLine();
+            Console.WriteLine("Adress: ");
+
+            NewOrder.OrderDate = null;
+            NewOrder.ShipDate = null;
+            NewOrder.DeliveryDate = null;
+
+
+
+            Console.WriteLine("Adding a new Base Station...");
+            ///צריך "לשמור את הישות פה"
+
+            Console.WriteLine("The new Base Station was successfully added\n");
+
+        }
+        #endregion
+
+        #region Update Order
+        public static void UpdateOrder()
+        {
+
+
+        }
+        #endregion
+
+        #region Show Order
+        public static void ShowOrder()
+        {
+
+        }
+
+        #endregion
+
+        #region Show Order List
+        public static void ShowOrderList()
+        {
+
+        }
+        #endregion
+
+        #region Delete Order
+        public static void DeleteOrder()
+        {
+
+        }
+        #endregion
+
+        #region OrderMenu
+        public static void OrderMenu()
+        {
+            Dal.OrderMenu Orderchoise = Dal.OrderMenu.AddOrder;
+            while (!Orderchoise.Equals(Dal.EntitysMenu.Exit))
+            {
+                Console.WriteLine("for add Order - press 1\n" +
+            "for Update Order - press 2\n" +
+            "for show order - press 3\n" +
+            "for show order list - press 4\n" +
+            "for delete order - press 5\n");
+            }                          
+            switch (Orderchoise)
+            {
+                case Dal.OrderMenu.AddOrder:
+                    AddNewOrder();
+                    break;
+                case Dal.OrderMenu.UpdateOrder:
+                    UpdateOrder();
+                    break;
+                case Dal.OrderMenu.ShowOrder:
+                    ShowOrder();
+                    break;
+                case Dal.OrderMenu.ShowListOfOrder:
+                    ShowOrderList();
+                    break;
+                case Dal.OrderMenu.DeleteAnOrder:
+                    DeleteOrder();
+                    break;                
+                default:
+                    Console.WriteLine("This option does not exist, please try again\n");
+                    break;
+            }
+        }
+        #endregion
+    }
+}
