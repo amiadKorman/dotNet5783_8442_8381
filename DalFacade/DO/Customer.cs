5e92ae2e50c11185574a83744f63e5f53fce20f7
ron hodadedi <ronchaim318@gmail.com>
2022-10-30 16:59:11 +0200

    Add Customer.cs

---
+++ DalFacade/DO/Customer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO
+{
+    public struct Customer
+    {
+        /// <summary>
+        /// Customer ID
+        /// </summary>
+        public int CustomerId { get; set; }
+
+        /// <summary>
+        /// Customer name
+        /// </summary>
+        public string CustomerName { get; set; }
+
+        /// <summary>
+        /// Customer email
+        /// </summary>
+        public string CustomerEmail { get; set; }
+
+        /// <summary>
+        /// Customer address
+        /// </summary>
+        public string CustomerAdress { get; set; }
+
+        public override string ToString() => $@"
+        Customer ID: {ID}, 
+        Customer Name: {CustomerName},
+        Customer Email: {CustomerEmail},
+        Customer Adress: {CustomerAdress}";  
+    }
+
+    
+}
