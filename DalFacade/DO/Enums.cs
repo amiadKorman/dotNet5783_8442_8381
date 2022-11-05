cacaceffcf10e30c52400ff03abf8c736e5d92ef
Amiad Korman <amiadk16@gmail.com>
2022-11-05 23:38:53 +0200

    Add names, cities and streets enumorations

--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -1,3 +1,6 @@
 ﻿namespace DO;
 
 public enum CategoryOfProduct { Phones, Laptops, Screens, Televisions, Accessories }
+public enum Names { Yossef, Shira, Michal, Dina, Elhanan, Osher, Miryam, Ester, Daniel, Nathan }
+public enum Cities { Jerusalem, Haifa, Naharia, Yeruham, Merav}
+public enum Streets { Hertzel, HaAtzmaot, Hachalotz, Yaffo}
