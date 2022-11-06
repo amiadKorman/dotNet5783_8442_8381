2de007c0a2926506d6308608e0e2e2cbfd232766
Amiad Korman <amiadk16@gmail.com>
2022-11-06 15:36:46 +0200

    Create CRUD methods structure for DalOrder

--- DalList/DalOrder.cs
+++ DalList/DalOrder.cs
@@ -1,5 +1,49 @@
-﻿namespace Dal;
+﻿using DO;
+namespace Dal;
 
 public class DalOrder
 {
+    /// <summary>
+    /// Add new order
+    /// </summary>
+    /// <param name="newOrder"></param>
+    /// <exception cref="NotImplementedException"></exception>
+    public void addOrder(Order newOrder)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Return order by given ID
+    /// </summary>
+    /// <param name="orderID"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public Order getOrder(int orderID)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Update order by given ID
+    /// </summary>
+    /// <param name="newOrder"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public Order updateOrder(Order newOrder)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Delete order by given ID
+    /// </summary>
+    /// <param name="orderID"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public Order deleteOrder(int orderID)
+    {
+        throw new NotImplementedException();
+    }
+
 }
