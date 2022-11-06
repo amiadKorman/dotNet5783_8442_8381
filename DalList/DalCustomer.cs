25bea3bf511c41f0ceebc7cabf87e20c51cadd76
Amiad Korman <amiadk16@gmail.com>
2022-11-06 16:22:41 +0200

    Create CRUD methods structure for DalCustomer

---
+++ DalList/DalCustomer.cs
@@ -0,0 +1,48 @@
+﻿using DO;
+
+namespace Dal;
+
+public class DalCustomer
+{
+    /// <summary>
+    /// Add new customer item
+    /// </summary>
+    /// <param name="newCustomer"></param>
+    /// <exception cref="NotImplementedException"></exception>
+    public void AddCustomer(Customer newCustomer)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Return order by given
+    /// </summary>
+    /// <param name="customerID"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public Customer GetCustomer(int customerID)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Update order item by given ID
+    /// </summary>
+    /// <param name="newCustomer"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public Customer UpdateOrderItem(Customer newCustomer)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Delete customer by given ID
+    /// </summary>
+    /// <param name="customerID"></param>
+    /// <exception cref="NotImplementedException"></exception>
+    public void DeleteCustomer(int customerID)
+    {
+        throw new NotImplementedException();
+    }
+}
