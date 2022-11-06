68993c15e33ffcb7ce4050d70ce5cca52f22c27f
Amiad Korman <amiadk16@gmail.com>
2022-11-06 16:12:42 +0200

    Create CRUD methods structure for DalProduct

--- DalList/DalProduct.cs
+++ DalList/DalProduct.cs
@@ -1,5 +1,49 @@
-﻿namespace Dal;
+﻿using DO;
+
+namespace Dal;
 
 public class DalProduct
 {
+    /// <summary>
+    /// Add new product item
+    /// </summary>
+    /// <param name="newProdect"></param>
+    /// <exception cref="NotImplementedException"></exception>
+    public void AddProduct(OrderItem newProdect)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Return product by given
+    /// </summary>
+    /// <param name="productID"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public Order GetProduct(int productID)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Update product item by given ID
+    /// </summary>
+    /// <param name="newProduct"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public Product UpdateOrderItem(Product newProduct)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Delete product item by given ID
+    /// </summary>
+    /// <param name="productID"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public Product DeleteOrderItem(int productID)
+    {
+        throw new NotImplementedException();
+    }
 }
