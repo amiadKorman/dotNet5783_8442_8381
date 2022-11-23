75f6db782db267a76fd58311c7681735fdd3f4cf
Amiad Korman <amiadk16@gmail.com>
2022-11-23 09:28:23 +0200

    Add DalList

---
+++ DalList/DalList.cs
@@ -0,0 +1,17 @@
+﻿using DalApi;
+
+namespace Dal;
+
+sealed public class DalList : IDal
+{
+    public static IDal Instance { get; } = new DalList();
+    private DalList() { }
+
+    public ICustomer Customer => new DalCustomer();
+
+    public IOrder Order => new DalOrder();
+
+    public IOrderItem OrderItem => new DalOrderItem();
+
+    public IProduct Product => new DalProduct();
+}
