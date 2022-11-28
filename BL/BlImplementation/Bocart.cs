83dc9cee33f4ea5199443faa7986fc2cef6a8509
ron hodadedi <hodadedi@g.jct.ac.il>
2022-11-28 17:40:44 +0200

    add new folder and Bocart

---
+++ BL/BlImplementation/Bocart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlImplementation
+{
+    internal class Bocart : IBocart
+    {
+        public Bocart() { }
+        public Bocart(string name) { }
+        
+    }
+}
