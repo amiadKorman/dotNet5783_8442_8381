83b950bce9142d2f25b5e9ef7a65ac1ea7d08930
ron hodadedi <hodadedi@g.jct.ac.il>
2022-11-28 18:37:10 +0200

    Add BL menu

---
+++ BlTest/BlMain.cs
@@ -0,0 +1,45 @@
+﻿using Bl;
+using BlApi;
+
+
+namespace BlTest;
+
+internal class BlMain
+{
+    #region Mainaly Main
+    public static void Main(string[] args)
+    {
+        EntitysMenu MenuChoise = EntitysMenu.OrderMenu;
+        while (!MenuChoise.Equals(EntitysMenu.Exit))
+        {
+            MenuChoise = (EntitysMenu)BlSafeInput.IntegerInput(
+                "For Order Menu - press 1\n" +
+                "For Order Item Menu - press 2\n" +
+                "For Product Menu - press 3\n" +
+                "For Customer Menu - press 4\n" +
+                "For Exit - press 0\n\n");
+
+            switch (MenuChoise)
+            {
+                case EntitysMenu.OrderMenu:
+                    BlMenuOfOrder.OrderMenu();
+                    break;
+                case EntitysMenu.OrderItemsMenu:
+                    BlMenuOfOrderItems.OrderItemMenu();
+                    break;
+                case EntitysMenu.ProductMenu:
+                    BlMenuOfProduct.ProductMenu();
+                    break;
+                case EntitysMenu.CustomerMenu:
+                    MenuOfCustomer.CustomerMenu();
+                    break;
+                case EntitysMenu.Exit:
+                    break;
+                default:
+                    Console.WriteLine("Error. Try again");
+                    break;
+            }
+        }
+    }
+    #endregion
+}
