91c09112ea77a4c2e2fd329d9f6d24e625d8cf41
ron hodadedi <hodadedi@g.jct.ac.il>
2022-12-01 15:47:57 +0200

    edite the names of Enums

---
+++ BlTest1/BlEnumsMenu.cs
@@ -0,0 +1,119 @@
+﻿using BO;
+using BlTest1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlTest1
+{
+
+    #region Entitys's menu 
+    /// <summary>
+    /// Enums for the Main program
+    /// </summary>
+    public enum EnumsEntitysMenu
+    {
+        [Description("Get to Order menu")]
+        OrderMenu = 1,
+        [Description("Get to OrderItem menu")]
+        OrderItemsMenu = 2,
+        [Description("Get to Product menu")]
+        ProductMenu = 3,
+        [Description("Get to Customer menu")]
+        CustomerMenu = 4,
+        [Description("Exit from program")]
+        Exit = 0,
+    }
+    #endregion
+
+    #region Order Menu
+    /// <summary>
+    /// Enus for the Menu of the Order menu
+    /// </summary>
+    public enum EnumsOrderMenu
+    {
+        [Description("Add an Order")]
+        AddOrder = 1,
+        [Description("Update an Order")]
+        UpdateOrder = 2,
+        [Description("Show an Order")]
+        ShowOrder = 3,
+        [Description("Show Orders List")]
+        ShowListOfOrder = 4,
+        [Description("Delete an Order")]
+        DeleteAnOrder = 5,
+        [Description("To Return Back To The Main Menu")]
+        GoBack = 0
+    }
+    #endregion
+
+    #region Order Item Menu
+    /// <summary>
+    /// Enums for the Menu of the Order Item menu
+    /// </summary>
+    public enum EnumsOrderItemMenu
+    {
+        [Description("Add an Order item")]
+        AddOrderItems = 1,
+        [Description("Update an Order item")]
+        UpdateOrderItems = 2,
+        [Description("Show an Order item by ID")]
+        ShowOrderItemByID = 3,
+        [Description("Show an Order item by order and product ID's")]
+        ShowOrderItemByProductAndCustomerID = 4,
+        [Description("Show Order items list")]
+        ShowListOfOrderItems = 5,
+        [Description("Show an specific Order's order items list")]
+        ShowOrderItemsListOfSpecificOrder = 6,
+        [Description("Delete an Order item")]
+        DeleteAnOrderItems = 7,
+        [Description("To Return Back To The Main Menu")]
+        GoBack = 0
+    }
+    #endregion
+
+    #region Product Menu
+    /// <summary>
+    /// Enus for the Menu of the Product menu
+    /// </summary>
+    public enum EnumsProductMenu
+    {
+        [Description("Add a Product")]
+        AddProduct = 1,
+        [Description("Update a Product")]
+        UpdateProduct = 2,
+        [Description("Show a Product")]
+        ShowProduct = 3,
+        [Description("Show Products List")]
+        ShowListOfProduct = 4,
+        [Description("Delete a Product")]
+        DeleteAProduct = 5,
+        [Description("To Return Back To The Main Menu")]
+        GoBack = 0
+    }
+    #endregion
+
+    #region Customer Menu
+    /// <summary>
+    /// Enus for the Menu of the customer menu
+    /// </summary>
+    public enum EnumsCustomerMenu
+    {
+        [Description("Add an Customer")]
+        AddCustomer = 1,
+        [Description("Update an Customer")]
+        UpdateCustomer = 2,
+        [Description("Show an Customer")]
+        ShowCustomer = 3,
+        [Description("Show Customers List")]
+        ShowListOfCustomers = 4,
+        [Description("Delete an Customer")]
+        DeleteCustomer = 5,
+        [Description("To Return Back To The Main Menu")]
+        GoBack = 0
+    }
+    #endregion
+}
