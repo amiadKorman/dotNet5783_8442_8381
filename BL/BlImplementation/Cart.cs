9edd56017757e09f183d753ff8f02609f3948ce3
Amiad Korman <amiadk16@gmail.com>
2022-12-11 14:32:16 +0200

    keep changing things

--- BL/BlImplementation/Cart.cs
+++ BL/BlImplementation/Cart.cs
@@ -5,8 +5,14 @@ namespace BlImplementation;
 internal class Cart : ICart
 {
     private DalApi.IDal dal = new Dal.DalList();
-
-    public BO.Cart Add(BO.Cart cart, int productId)
+    internal class Config
+    {
+        private const int startItemsNumber = 10000;
+        private static int ItemID = startItemsNumber;
+        internal static int NextItemID { get => ItemID++; }
+    }
+   
+public BO.Cart Add(BO.Cart cart, int productId)
     {
         //check if the validation of the given product ID
         if (productId < 100000 || productId >= 1000000)
@@ -29,8 +35,9 @@ internal class Cart : ICart
             }
             else
             {
-                cart.Items.Append(new BO.OrderItem
+                cart.Items?.Append(new BO.OrderItem
                 {
+                    ID = Config.NextItemID,
                     ProductID = productId,
                     Name = product.Name,
                     Price = product.Price,
