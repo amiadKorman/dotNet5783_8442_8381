83ea6d6f842aa5485519409aad495778d74521e9
Amiad Korman <amiadk16@gmail.com>
2023-01-19 09:24:08 +0200

    Start update DalList to Singleton

--- DalList/DalList.cs
+++ DalList/DalList.cs
@@ -2,8 +2,12 @@
 
 namespace Dal;
 
-sealed public class DalList : IDal
+internal sealed class DalList : IDal
 {
+    private DalList() { }
+    
+    public static IDal Instance { get; } = new DalList();
+    
     public ICustomer Customer { get; } = new DalCustomer();
 
     public IOrder Order { get; } = new DalOrder();
