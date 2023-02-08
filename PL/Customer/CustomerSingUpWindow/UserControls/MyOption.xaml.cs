fa10697ad6958db170d318ba6e53d4173eb99fa4
ron hodadedi <hodadedi@g.jct.ac.il>
2023-02-08 12:04:01 +0200

    customer sing up

---
+++ PL/Customer/CustomerSingUpWindow/UserControls/MyOption.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL.Customer.CustomerSingUpWindow.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MyOption.xaml
+    /// </summary>
+    public partial class MyOption : Window
+    {
+        public MyOption()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public FontAwesome.WPF.FontAwesomeIcon Icon
+        {
+            get { return (FontAwesome.WPF.FontAwesomeIcon)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(MyOption));
+
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(FontAwesome.WPF.FontAwesomeIcon), typeof(MyOption));
+
+
+
+
+
+    }
+}
