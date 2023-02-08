49314b44710f389a5ff52ee660e657071d30e2f7
ron hodadedi <hodadedi@g.jct.ac.il>
2023-02-08 22:11:52 +0200

    admin

--- PL/Admin/MenuWindowMenager/MenuWindowMenager.xaml.cs
+++ PL/Admin/MenuWindowMenager/MenuWindowMenager.xaml.cs
@@ -23,5 +23,35 @@ namespace PL.Admin.MenuWindowMenager
         {
             InitializeComponent();
         }
+
+        private bool IsMaximize = false;
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (IsMaximize)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1080;
+                    this.Height = 720;
+
+                    IsMaximize = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+
+                    IsMaximize = true;
+                }
+            }
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
     }
 }
